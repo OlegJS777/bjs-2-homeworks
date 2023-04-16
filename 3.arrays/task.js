1e9fe425422e36c19580ce2f200de69537717d89
OlegJS777 <121798947+OlegJS777@users.noreply.github.com>
2023-04-16 13:58:01 +0300

    Add files via upload

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
+    return (arr1.every((item, idx) => item === arr2[idx])) && (arr1.length === arr2.length)
 }
+console.log(compareArrays);
+
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let output = users.filter(users => users.gender === gender).map(users => users.age).reduce((acc, item, idx, arr)=> {
+        acc += item;
+        if (idx === arr.length - 1) {
+            return acc / arr.length;
+        }
+        return acc; 
+    }, 0) 
+    return output;
+
+    console.log(getUsersNamesInAgeRange);
 }
