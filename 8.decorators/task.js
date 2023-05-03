f25dc20ae4f6645d6873275df46c7010919a4ce5
OlegJS777 <121798947+OlegJS777@users.noreply.github.com>
2023-05-03 19:00:07 +0300

    Add files via upload

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,41 @@
-//Задача № 1
 function cachingDecoratorNew(func) {
-  
-}
+    let cache = [];
+    function wrapper(...args) {
+        const hash = md5(args); 
+        let objectInCache = cache.find((item) => item.hash === hash); 
+            if (objectInCache) { 
+                console.log("Из кэша: " + objectInCache.result); 
+                    return "Из кэша: " + objectInCache.result;
+        }
+        let result = func(...args); 
+            cache.push({hash, result}) ; 
+                 if (cache.length > 5) { 
+          cache.shift() 
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;  
+    }
+    return wrapper;
+    }
 
-//Задача № 2
+
 function debounceDecoratorNew(func, delay) {
-  
+  let timeOut = null;
+  function wrapper(...args) {
+    wrapper.allCount += 1;
+         if(timeOut){
+              clearInterval(timeOut);
+    }
+    if (timeOut === null) {
+          wrapper.count += 1;
+             func(...args);
+    }
+    timeOut = setTimeout(() => {
+         wrapper.count += 1;
+            func(...args);
+    },     delay);
+  }
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+  return wrapper;
 }
